628641b15b39df254f92e0c9271b0e328681715f
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-11 22:06:08 -0500

    Created partsNeeded variable

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,6 +5,9 @@ console.log('****** Part Supply *******');
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
 
+let partsNeeded = 40;
+console.log(partsNeeded);
+
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
